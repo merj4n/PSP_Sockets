43ae8507657d145da76b852d5089e9f4f2705fd8
merjan <merjan@gmail.com>
2018-11-26 21:16:39 +0100

    Ejercicio 5 update

--- Ejercicio5/src/ListarArchivos.java
+++ Ejercicio5/src/ListarArchivos.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 public class ListarArchivos {
 
+
     public ListarArchivos() { }
 
-    public List<String> buscador(String nombre, File raiz){
+    public static List<String> buscador(String nombre, File raiz){
         List<String> listaArchivos = new ArrayList<>();
         File[] lista = raiz.listFiles();
 
