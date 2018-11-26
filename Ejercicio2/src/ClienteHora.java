6e76f74f33d0dbc5d9466bb38113cced0744805c
merjan <merjan@gmail.com>
2018-11-26 11:43:01 +0100

    Actualizacion

---
+++ Ejercicio2/src/ClienteHora.java
@@ -0,0 +1,42 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClienteHora {
+    public static void main(String[] args) {
+
+        Socket cliente = null;
+        DataInputStream entrada;
+        DataOutputStream salida;
+        String cadena;
+
+        try{
+            // Crear socket cliente
+            cliente= new Socket("localhost",9999);
+
+            // Obtener los InputStream y/o OutputStream del servidor
+            entrada = new DataInputStream (cliente.getInputStream());
+            salida = new DataOutputStream (cliente.getOutputStream());
+
+            // Acciones
+            salida.writeUTF("Hora?");
+            salida.writeUTF("Bye");
+            while (true) {
+                cadena = entrada.readUTF();
+                if (cadena.equals("Bye")){
+                    break;
+                }else {
+                    System.out.println(cadena);
+                }
+            }
+            // Cerrar los canales de entrada, salida y el socket cliente
+            salida.close();
+            entrada.close();
+            cliente.close();
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ Ejercicio2/src/Servidor.java
@@ -0,0 +1,71 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Servidor {
+    public static void main(String[] args) {
+        ServerSocket servicioEcho = null;
+        ServerSocket servicioHora = null;
+        Socket socketEcho = null;
+        Socket socketHora = null;
+        DataInputStream entradaEcho,entradaHora;
+        DataOutputStream salidaEcho,salidaHora;
+        String cadena,cadena2;
+
+        try{
+            // Crear socket servidor
+            servicioEcho= new ServerSocket(9998);
+            servicioHora= new ServerSocket(9999);
+
+            // Aceptar un cliente
+            socketEcho= servicioEcho.accept();
+            socketHora= servicioHora.accept();
+            // Obtener los InputStream y/o OutputStream del servidor
+            entradaEcho = new DataInputStream (socketEcho.getInputStream());
+            entradaHora = new DataInputStream (socketHora.getInputStream());
+            salidaEcho = new DataOutputStream (socketEcho.getOutputStream());
+            salidaHora = new DataOutputStream (socketHora.getOutputStream());
+
+            // Acciones a realizar por el servidor
+            while (true) {
+                cadena = entradaEcho.readUTF();
+                cadena2 = entradaHora.readUTF();
+                System.out.println(cadena);
+
+                if (cadena.equals("Bye")) {
+                        salidaEcho.writeUTF("Adios");
+                        salidaEcho.close();
+                        entradaEcho.close();
+                        socketEcho.close();
+                        break;
+                    } else {
+                        salidaEcho.writeUTF(cadena);
+                }
+                if (cadena2.equals("Bye")) {
+                        salidaHora.writeUTF("Adios");
+                        salidaHora.close();
+                        entradaHora.close();
+                        socketHora.close();
+                        break;
+                    } else {
+                        salidaHora.writeUTF(FechaHora());
+                    }
+                }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+    public static String FechaHora() {
+
+        Date date = new Date();
+//Caso 1: obtener la hora y salida por pantalla con formato:
+        DateFormat hourFormat = new SimpleDateFormat("HH:mm:ss");
+        String hora="Hora: "+hourFormat.format(date);
+        return hora;
+    }
+}
