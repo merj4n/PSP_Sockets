f4bffdde5aa3ba54bd5331b39cb97a31abeeee79
merjan <merjan@gmail.com>
2018-11-27 12:26:27 +0100

    Ya transfiero correctamente.

--- Ejercicio5/src/FicheroCliente.java
+++ Ejercicio5/src/FicheroCliente.java
@@ -1,25 +1,53 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class FicheroCliente {
     public static void main(String[] args) {
         DataInputStream dis;
+        DataOutputStream dos;
         Socket socketCliente = null;
-        String cadena;
+        String cadena="-1";
+        String opcion="";
+        BufferedInputStream bis;
+        BufferedOutputStream bos;
+        int in;
+        byte[] receivedData;
         int i=0;
+        int total;
 
         try{
             socketCliente = new Socket("localhost", 4444);
             dis = new DataInputStream (socketCliente.getInputStream());
-            while (true){
+            dos = new DataOutputStream(socketCliente.getOutputStream());
+            cadena =dis.readUTF();
+            total=Integer.parseInt(cadena);
+            while (i!=total){
                 cadena = dis.readUTF();
                 System.out.println(i+" "+cadena);
                 i++;
-                if(cadena.isEmpty()) {
-                    socketCliente.close();
-                    dis.close();
-                }
+
             }
+            System.out.println("Elige un numero de fichero: ");
+            Scanner sc = new Scanner(System.in);
+            //Eligo el fichero a guardar
+            opcion = sc.nextLine();
+            //Envio el numero de fichero
+            dos.writeUTF(opcion);
+            //Obtengo el nombre del fichero elegido
+            String filename=dis.readUTF();
+            //Defino el tamaño de los datos a recibir
+            receivedData = new byte[1024];
+            //Creo los buffer de entrada-salida de datos
+            bis = new BufferedInputStream(socketCliente.getInputStream());
+            //Para guardar fichero recibido
+            bos = new BufferedOutputStream(new FileOutputStream("C:/Users/merjan/Desktop/casa/"+filename));
+            while ((in = bis.read(receivedData)) != -1){
+                bos.write(receivedData,0,in);
+            }
+            bos.close();
+            dis.close();
+            socketCliente.close();
         }catch(IOException e){
             System.err.println("No puede establecer conexion");
         }
--- Ejercicio5/src/FicheroServidor.java
+++ Ejercicio5/src/FicheroServidor.java
@@ -8,22 +8,68 @@ public class FicheroServidor {
     public static void main (String args[]) {
 
         DataOutputStream dos;
+        DataInputStream dis;
         ServerSocket servidor = null;
         Socket cliente;
+        boolean bucle=true;
+        String cadena="";
+        BufferedInputStream bis;
+        BufferedOutputStream bos;
+
+        byte[] byteArray;
+        int in;
+        String file;
 
         try{
             servidor = new ServerSocket(PORT);
             cliente = servidor.accept();
-            dos = new DataOutputStream (cliente.getOutputStream());
-            List<String> lista;
-            ListarArchivos find = new ListarArchivos();
-            lista=find.buscador("", new File("C:/Users/merjan/Desktop/file"));
-            for (String elem:lista) {
-                dos.writeUTF(elem);
-            }
-
-        }catch(IOException e){
-            System.out.println("Error al conectar con el servidor");
+            dos = new DataOutputStream(cliente.getOutputStream());
+            dis = new DataInputStream(cliente.getInputStream());
+            while (bucle) {
+                List<String> lista;
+                ListarArchivos find = new ListarArchivos();
+                lista = find.buscador("", new File("C:/Users/merjan/Desktop/file"));
+                String total=String.valueOf(lista.size());
+                dos.writeUTF(total);
+                for (String elem : lista) {
+                    dos.writeUTF(elem);
+                }
+                while (true) {
+                    //Obtengo el numero de fichero a enviar
+                    cadena = dis.readUTF();
+                    //Si es -1 salimos del bucle
+                    if (cadena.equals("-1")) {
+                        break;
+                    }
+                    else{
+                        //Recibimos el nombre del fichero
+                        file = lista.get(Integer.parseInt(cadena));
+                        //Envio nombre fichero al cliente
+                        dos.writeUTF(file);
+                        //Defino ruta del fichero
+                        final File localFile = new File( "C:/Users/merjan/Desktop/file/"+file );
+                        //Defino el buffer de entrada-salida del fichero
+                        bis = new BufferedInputStream(new FileInputStream(localFile));
+                        bos = new BufferedOutputStream(cliente.getOutputStream());
+                        //Enviamos el fichero
+                        byteArray = new byte[8192];
+                        while ((in = bis.read(byteArray)) != -1){
+                            bos.write(byteArray,0,in);
+                        }
+                    }
+                    bos.close();
+                    bis.close();
+                    dis.close();
+                    bucle=false;
+                }
+            cliente.close();
+            servidor.close();
+            dos.close();
         }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 }
